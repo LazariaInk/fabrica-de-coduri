673d8bb49c63d399cc9610ef7f2e92f9c56b9709
LazariaInk <lorienlored@gmail.com>
2025-03-19 13:26:53 +0300

    add main class to git

---
+++ src/main/java/com/lazar/fabrica_de_coduri/FabricaDeCoduriApplication.java
@@ -0,0 +1,13 @@
+package com.lazar.fabrica_de_coduri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaDeCoduriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaDeCoduriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lazar/fabrica_de_coduri/FabricaDeCoduriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazar.fabrica_de_coduri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricaDeCoduriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
